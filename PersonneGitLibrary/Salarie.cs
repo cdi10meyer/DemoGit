ac383519ccefb4c0488ca0f3165f990cd2873a33
Fabien Allard <fabien.allard.83@orange.fr>
2018-11-08 15:02:35 +0100

    Ajout classe salaire

---
+++ PersonneGitLibrary/Salarie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonneGitLibrary
+{
+   public class Salarie : Personne
+  {
+
+    public Salarie(string nom, string prenom, int age, int salaire, int matricule): base(nom, prenom, age)
+    {
+      Salaire = salaire;
+      Matricule = matricule;
+    }
+
+    public int Salaire { get; set; }
+
+    public int Matricule { get; set; }
+  }
+}
